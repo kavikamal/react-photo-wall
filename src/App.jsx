f95e748f550c08940b0828fe23f7d41f2c9016e7
Kavitha Kamal <kavithakamal@gmail.com>
2018-05-24 16:11:47 -0400

    Latest code

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ const PHOTO_URL = "https://picsum.photos/200?photo=";
 const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
-  // 1. Declare a state object that will be used to track an array of photos
+  // 1. Declare a state object that will be used to track an array of photos -- done
   state = {
+     photosArr : []
   }
 
   // 2. Declare a life cycle method
@@ -17,8 +18,19 @@ class App extends Component {
   //  - will be called after the component is initially rendered
   // - will fetch an array of photos
   // - will add that array of photos to state once received
+  componentDidMount() {
+    fetch(PHOTO_LIST_URL)
+      .then((res) => { return res.json() })
+            .then((data) => { 
+      this.setState({photosArr : data}) 
+      console.log(this.state.photosArr);
+    });
+  }
+
   render() {
-    const { photos = [] } = this.state;
+    const  photos =  this.state.photosArr;
+  console.log(photos);
+
     return (
       <React.Fragment>
         <header>
@@ -33,10 +45,11 @@ class App extends Component {
             {/* We use map here because Array.prototype.map is an expression,
               * and for loops are not. You'll learn more about this soon! 
               */}
-            {photos.map( photo => 
-                <img alt={/* 3. Fill me in with the photo's filename */ ""}
-                     key={/* 4. Fill me in with the photo's id */ ""}
-                     src={/* 5. Fill me in with the photo's URL */ ""}
+
+            {photos.map( photo =>    
+                <img alt={/* 3. Fill me in with the photo's filename */ photo.filename}
+                     key={/* 4. Fill me in with the photo's id */ photo.id}
+                     src={/* 5. Fill me in with the photo's URL */ PHOTO_URL+photo.post_url}
                 />
             )}
         </div>
